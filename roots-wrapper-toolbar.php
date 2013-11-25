1ec3399a3deac7c05da0c9ec399535dab1a9524f
Foxaii <nick@foxaii.com>
2013-11-25 20:46:05 +0000

    First commit.

---
+++ roots-wrapper-toolbar.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name:  Roots Wrapper Toolbar
+Plugin URI:   http://github.com/roots/roots-wrapper-toolbar
+Description:  A WordPress plugin (or mu-plugin) that displays the base and sidebar templates selected by the Roots wrapper in the WordPress toolbar. Requires the <a href="http://roots.io/">Roots</a> theme and wrapper.
+Version:      0.9
+Author:       Nick Fox
+Author URI:   http://roots.io/
+License:      MIT
+*/
+
+defined('ABSPATH') or die('A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools. - Douglas Adams');
+
+if (!class_exists('Roots_Wrapper_Toolbar')) {
+
+  class Roots_Wrapper_Toolbar {
+    private static $_single; // A singleton will prevent multiple toolbar dropdowns.
+    public static $parent; // Use as the node parent if you want to add more dropdowns to the group.
+  
+    function __construct() {
+      if (!isset(self::$_single)) {
+        self::$_single = $this; // Singleton set.
+        add_action('admin_bar_menu', array('Roots_Wrapper_Toolbar', 'init'), 999); // Wait until init to do our checks.
+      }
+    }
+
+    public function init($toolbar) {
+    if (is_admin() | !is_super_admin() || !is_admin_bar_showing() || !class_exists('Roots_Wrapping')) return; // Use is_super_admin for network support.
+
+      global $template;
+
+      $main_template  = Roots_Wrapping::$main_template; // Chosen by the WordPress template hierarchy.
+      $base_template  = $template; // Base file selected by the Roots Wrapper.
+      self::$parent   = 'rwtb-templates'; // The id for the dropdown group.
+
+      $top = array(
+        'id'    => 'rwtb',
+        'title' => __('Roots', 'roots'),
+        'meta'  => array('class' => 'roots-toolbar')
+      );
+
+      $group = array(
+        'parent' => 'rwtb',
+        'id'     => self::$parent,
+        'meta'   => array('class' => 'roots-templates')
+      );
+
+      $main = array(
+        'parent' => self::$parent,
+        'id'     => 'rwtb-main',
+        'title'  => __('Main: ', 'roots') . basename($main_template),
+        'meta'   => array('class' => 'roots-toolbar', 'title' => esc_url($main_template))
+      );
+
+      global $template;
+      $base = array(
+        'parent' => self::$parent,
+        'id'     => 'rwtb-base',
+        'title'  => __('Base: ', 'roots') . basename($base_template),
+        'meta'   => array('class' => 'roots-toolbar', 'title' => esc_url($base_template))
+      );
+
+      $toolbar->add_node($top); // Add the top level, group and template nodes to the toolbar.
+      $toolbar->add_group($group);
+      $toolbar->add_node($main);
+      $toolbar->add_node($base);
+    }
+  }
+
+  new Roots_Wrapper_Toolbar();
+}
+
+?>
